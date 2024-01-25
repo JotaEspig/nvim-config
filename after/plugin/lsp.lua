1ad37dc728e15cae2dd621e4816e491b4c594bb5
João Vitor Espig <jotaespig@gmail.com>
2024-01-25 13:22:27 -0300

    feat(lsp): add mapping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,6 +1,18 @@
+local vim = vim
 local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
+
+lsp.on_attach(function (client, buffnr)
+	local opts = {buffer = buffnr, remap = false}
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "<leader>q", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+end)
+
 lsp.setup()
 
 -- Installs language servers
