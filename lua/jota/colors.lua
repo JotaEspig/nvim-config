e9dc47351a15e0b72287d78bc1e2b9cf61516fbb
João Vitor Espig <jotaespig@gmail.com>
2024-01-25 12:04:59 -0300

    add more backgrounds and renamed function

--- lua/jota/colors.lua
+++ lua/jota/colors.lua
@@ -1,16 +1,19 @@
 local vim = vim
 
-function ApplyTransparentBG()
+function OverrideBG()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NonText", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#11131a" })
 end
 
 function ApplyColors(color)
 	color = color or "tokyonight"
 	vim.cmd.colorscheme(color)
 
-	ApplyTransparentBG()
+	OverrideBG()
 end
 
 ApplyColors()
