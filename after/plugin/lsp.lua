ee9d4e66042704e3d0c1e9faa03ed08bb9d2ab13
João Vitor Espig <jotaespig@gmail.com>
2024-01-26 00:31:50 -0300

    feat(lsp): mapping for lsp.hover()

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,6 +7,7 @@ lsp.on_attach(function (client, buffnr)
 	local opts = {buffer = buffnr, remap = false}
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>q", function() vim.diagnostic.open_float() end, opts)
 	vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>rf", function() vim.lsp.buf.references() end, opts)
