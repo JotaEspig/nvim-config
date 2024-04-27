b72e6f35831b79488bcdf0c8149ab3fa722f1300
João Vitor Espig <jotaespig@gmail.com>
2024-04-27 13:15:26 -0300

    feat: add map to find all files in the projects

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,10 @@
+local vim = vim
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fa', function ()
+    builtin.find_files({ hidden=true, no_ignore=true })
+end)
+vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Grep> ") })
 end)
