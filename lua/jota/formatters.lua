2b12ba1ecdd7a7b3285a35c7236ab1f4969b02b7
João Vitor Espig <jotaespig@gmail.com>
2024-05-10 21:21:52 -0300

    fix bug in clang-format

--- lua/jota/formatters.lua
+++ lua/jota/formatters.lua
@@ -1,17 +1,6 @@
 local vim = vim
 
-vim.api.nvim_exec([[
-function FormatBuffer()
-    if &modified && executable("clang-format") && !empty(findfile('.clang-format', expand('%:p:h') . ';'))
-        let cursor_pos = getpos('.')
-        :%!clang-format
-        call setpos('.', cursor_pos)
-    endif
-endfunction
-]],
-false)
-
-vim.cmd("autocmd BufWritePre *.h,*.hpp,*.c,*.cpp,*.vert,*.frag,*.glsl :call FormatBuffer()")
+vim.cmd("autocmd BufWritePost *.h,*.hpp,*.c,*.cpp,*.vert,*.frag,*.glsl :lua vim.lsp.buf.format()")
 vim.cmd("autocmd BufWritePost *.go silent !gofmt -s -w .")
 vim.cmd("autocmd BufWritePost *.go silent !goimports -w .")
 vim.cmd("autocmd BufWritePost *.py silent !autopep8 % -i")
