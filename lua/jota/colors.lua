773ad8d01240e89ae0e91c577e4018a54d8f9d9b
João Vitor Espig <jotaespig@gmail.com>
2025-06-03 23:40:10 -0300

    feat: saving user theme and added vim command that autocomplete

--- lua/jota/colors.lua
+++ lua/jota/colors.lua
@@ -1,6 +1,7 @@
 local vim = vim
+local colorscheme_file = vim.fn.stdpath("state") .. "/.colorscheme"
 
-function OverrideBG()
+local function OverrideBG()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
@@ -9,11 +10,32 @@ function OverrideBG()
     vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#11131a" })
 end
 
-function ApplyColors(color)
-    color = color or "tokyonight"
-    vim.cmd.colorscheme(color)
-
+local function ApplyColors(colorscheme)
+    colorscheme = colorscheme or "tokyonight-moon"
+    vim.cmd.colorscheme(colorscheme)
     OverrideBG()
 end
 
-ApplyColors()
+local function SaveColors(colorscheme)
+    vim.fn.writefile({ colorscheme }, colorscheme_file)
+    ApplyColors(colorscheme)
+end
+
+vim.api.nvim_create_user_command("SetTheme", function(opts)
+    SaveColors(opts.args)
+end, {
+    nargs = "?",
+    complete = function()
+        return vim.fn.getcompletion("", "color")
+    end
+})
+
+-- if file exists sets it, otherwise sets default and save it
+if vim.fn.filereadable(colorscheme_file) == 1 then
+    local colorscheme = vim.fn.readfile(colorscheme_file)[1]
+    ApplyColors(colorscheme)
+else
+    local default_colorscheme = "tokyonight-moon"
+    ApplyColors(default_colorscheme)
+    vim.fn.writefile({ default_colorscheme }, colorscheme_file)
+end
