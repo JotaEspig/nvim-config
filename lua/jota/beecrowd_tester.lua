f7f06f27e662c9557d9240a7d256ab126758d03f
João Vitor Espig <jotaespig@gmail.com>
2025-06-14 00:49:11 -0300

    feat: add command to help with beecrowd tester

---
+++ lua/jota/beecrowd_tester.lua
@@ -0,0 +1,27 @@
+-- File that improves experience with repository: https://github.com/jotaEspig/beecrowd-tester
+local vim = vim
+
+vim.api.nvim_create_user_command("BeecrowdTest", function(opts)
+  local filename = opts.args
+  if filename == "" then
+    print("Usage: :BeecrowdTest <filename>")
+    return
+  end
+
+  -- Extract basename without extension
+  local base = filename:match("^(.*)%.") or filename
+
+  -- Construct file paths
+  local solution_file = "solutions/" .. filename
+  local input_file = "tests/" .. base .. ".in"
+  local expected_file = "tests/" .. base .. ".exout"
+
+  -- Open files in splits
+  vim.cmd("tabnew " .. solution_file)
+  vim.cmd("tabnew " .. input_file)
+  vim.cmd("tabnew " .. expected_file)
+end, {
+  nargs = 1,
+  complete = "file",
+  desc = "Open related beecrowd files in separate tabs"
+})
