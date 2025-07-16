e62aaab355b14725ff7cffe653a76777753ec4bd
João Vitor Espig <jotaespig@gmail.com>
2025-07-15 21:39:34 -0300

    removed some ensured lsps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -24,7 +24,7 @@ require("mason").setup({})
 require("mason-lspconfig").setup({
     -- Replace the language servers listed here
     -- with the ones you want to install
-    ensure_installed = { "clangd", "pyright", "gopls", "lua_ls", "elixirls"},
+    ensure_installed = { "clangd", "gopls", "lua_ls" },
     handlers = {
         lsp.default_setup,
     },
